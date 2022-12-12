2558a39e78e97a82da04ec5075fa72f7461a5a00
GIRARD GHISLAIN <girard.gh@gmail.com>
2022-12-12 12:03:12 +0100

    send quest 8

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-require("dotenv").config();
-
+const dotenv = require("dotenv").config();
 const express = require("express");
-
 const app = express();
 
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+
+
 
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
-
-app.get("/", welcome);
-
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-// app.post("/api/users", userHandlers.postUser);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 
-app.put("/api/users/:id", userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+  );
+  
+  const welcome = (req, res) => {
+    res.send("Welcome to my favourite movie list");
+  };
+  
+  app.get("/", welcome);
+  
+  app.use(verifyToken);
+  
+  app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+app.put("/api/movies/:id", verifyToken, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", verifyToken, movieHandlers.deleteMovie);
+
+app.post("/api/users", verifyToken, hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", verifyToken, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyToken, userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
